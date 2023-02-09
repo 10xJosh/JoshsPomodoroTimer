d446c5ab11a42764f0e605572b73f751fae8dc60
10xJosh <55113224+10xJosh@users.noreply.github.com>
2023-02-08 22:14:21 -0700

    Add project files.

---
+++ JoshsPomodoroTimer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JoshsPomodoroTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // This allows the window to be moved around when WindowStyle=None
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.DragMove();
+        }
+
+        private void exitButton(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
